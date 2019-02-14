c05d91c1225126b1053f8a53b8e16b93befa4a48
StarCat4Evr <45293175+StarCat4Evr@users.noreply.github.com>
2019-02-14 16:26:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,3 +19,23 @@ sprite():
 
 if "__name__" == main:
   main()
+  
+  
+class Menu(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("menuBar.gif")
+        self.image = pygame.transform.scale(self.image, (640, 16))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 0
+        self.rect.centery = 440
+    def update(self):
+        self.rect.centerx = 0
+        self.rect.centery = 440
+        #(screenX, screenY) = pygame.Surface(screen.get_size())
+        #XPos = screenX
+        #YPos = screenY
+        #self.rect.center = (XPos, YPos)
+        #print self.rect.centerx
+        #print "Y coordingate ", self.rect.centery
+        #print self.rect.center
