16f2acdef20bb2e548bd253e40e2b1a6b2704cb5
Emily Bauer <45293175+StarCat4Evr@users.noreply.github.com>
2019-03-18 09:10:16 -0400

    Update page1backend.js

--- page1backend.js
+++ page1backend.js
@@ -51,7 +51,7 @@ function checkSpeed() {
 	if (count!=prevCount){
 
 		$('#liveWpm').html(Math.round((count-prevCount) / (iTime - thenTime) * 60000, 2));
-		if (count!=prevCount){
+		if (count>=prevCount){
 			wordCurrency++;
 		}
 		prevCount = count;
@@ -82,15 +82,82 @@ function pad(val) {
 
 
 var canvas = document.getElementById("myCanvas");
-var ctv = canvas.getContext("2d");
-ctv.fillStyle = " #356D32"; //60A33D
-ctv.fillRect(0,0,600,400);
-
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#60A33D";
-ctx.fillRect(10,90, 280, 5);
+ctx.fillStyle = " #227C2F"; //60A33D, 52A02C, 227C2F
+ctx.fillRect(0,95,300,100);
+ctx.fillStyle = " #5EA6D0"; //60A33D
+ctx.fillRect(0,0,300,95);
+//300 wide, 150 tall?
+ctx.fillStyle = "#BDEB0A";
 ctx.fillRect(10,100, 280, 5);
 ctx.fillRect(10,110, 280, 5);
 ctx.fillRect(10,120, 280, 5);
 ctx.fillRect(10,130, 280, 5);
 ctx.fillRect(10,140, 280, 5);
+
+
+var row1=[];
+var row2=[];
+var row3=[];
+var row4=[];
+var row5=[];
+var row6=[];
+var rows = [row1, row2, row3, row4, row5, row6];
+var flower1 = [50, 3, ["resources/flower1/flower.1_1.png","resources/flower1/flower.1_2.png","resources/flower1/flower.1_3.png","resources/flower1/flower.1_4.png","resources/flower1/flower.1_5.png"]];
+var flower2 = [50, 4, ["resources/flower1/flower.1_1.png","resources/flower1/flower.1_2.png","resources/flower1/flower.1_3.png","resources/flower1/flower.1_4.png","resources/flower1/flower.1_5.png"]];
+var flower3 = [50, 2, ["resources/flower1/flower.1_1.png","resources/flower1/flower.1_2.png","resources/flower1/flower.1_3.png","resources/flower1/flower.1_4.png","resources/flower1/flower.1_5.png"]];
+var flower4 = [50, 4, ["resources/flower2/flower.2_1.png","resources/flower2/flower.2_2.png","resources/flower2/flower.2_3.png","resources/flower2/flower.2_4.png","resources/flower2/flower.2_5.png"]];
+
+var flowerSelection = [flower1, flower2,flower3];
+
+function init(){
+	for (i=0;i<rows.length;i++){
+		for (j=0;j<rows[i].length;j++){
+			//console.log(j);
+			var lol = rows[i];
+			[xPos, yPos, flowerType]=lol[j];
+			[countMin, current, img]=flowerType;
+			drawImg(img[current],xPos, yPos)
+			console.log(img[current]);
+		}
+	}
+}
+
+
+
+function drawImg(img, X, Y){
+	var base_image = new Image();
+	base_image.src = img;
+	base_image.onload = function(){
+		ctx.drawImage(base_image, X, Y-32);
+	}
+}
+
+//(xpos,ypos) = mouse.getPos
+//init();
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+function addFlowers(){
+	var em = getRandomInt(0, rows.length)
+	var i = rows[em];	//choose a row	
+	var len = getRandomInt(1, 28)*10;
+	var flow = getRandomInt(0, flowerSelection.length);
+	var y = em*10+100;
+	var tuple = [len, y, flowerSelection[flow]];
+	i.push(tuple);
+	//console.log(i);
+}
+
+for (p=0;p<30;p++){
+	addFlowers();
+}
+
+init();
+
+//make_base();
+
+
